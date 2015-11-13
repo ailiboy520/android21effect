f8ec2ad7822b3c88c58a703a33ec9af8c8651d00
zhangyang3233 <zhangyang3233@163.com>
2015-11-13 11:12:07 +0800

    .

---
+++ app/src/main/java/com/effect/zy/android50effect/demo/RippleActivity.java
@@ -0,0 +1,17 @@
+package com.effect.zy.android50effect.demo;
+
+import android.os.Bundle;
+
+import com.effect.zy.android50effect.R;
+import com.effect.zy.android50effect.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_ripple)
+public class RippleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
